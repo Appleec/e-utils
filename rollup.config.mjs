b32d0bb6286ea437020d2d0b26f9e731c82c4833
appleex <771992522@qq.com>
2025-02-17 14:18:09 +0800

    build: 优化打包配置与流程

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,182 +1,146 @@
 // rollup.config.mjs
-import typescript from '@rollup/plugin-typescript'
-import dts from 'rollup-plugin-dts'
-import esbuild, { minify } from 'rollup-plugin-esbuild'
-import externals from 'rollup-plugin-node-externals'
-
-// babel
-import resolve from '@rollup/plugin-node-resolve';
+import esbuild, { minify } from 'rollup-plugin-esbuild';
+import dts from 'rollup-plugin-dts';
+import nodeExternals from 'rollup-plugin-node-externals';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
+// Use `rollup-plugin-esbuild`
+// instead of `@rollup/plugin-typescript` and `@rollup/plugin-terser`
+// import typescript from '@rollup/plugin-typescript';
+// import terser from '@rollup/plugin-terser';
+
+// Controls
+const useCdnBuild = false; //
+const useEsbuild = true; // `true` -> use esbuild, `false` use tsc
+
+// Options
 const usePreferConst = false; // Use "const" instead of "var"
 const usePreserveModules = true; // `true` -> keep modules structure, `false` -> combine everything into a single file
 const useStrict = true; // Use "strict"
-const useThrowOnError = true; // On error throw and exception
 const useSourceMap = false; // Generate source map files
-const useEsbuild = true; // `true` -> use esbuild, `false` use tsc
 
 // https://rollup.nodejs.cn/
-export default [
-    // .d.ts build
-    {
-        input: 'src/index.ts',
-        output: {
-            file: 'dist/index.d.ts',
-            format: 'es'
-        },
-        plugins: [externals(), dts()]
-    },
-    // CJS build
-    {
-        input: 'src/index.ts',
-        output: {
-            dir: 'dist/cjs',
-            format: 'cjs',
-            generatedCode: {
-                constBindings: usePreferConst
+// https://rollup.nodejs.cn/configuration-options/
+export default () => {
+    const config = [
+        // .d.ts build
+        {
+            input: 'src/index.ts',
+            output: {
+                format: 'es',
+                dir: 'dist',
+                entryFileNames: '[name].d.ts',
             },
-            preserveModules: usePreserveModules,
-            strict: useStrict,
-            entryFileNames: '[name].js', // .cjs
-            sourcemap: useSourceMap
+            plugins: [nodeExternals(), dts()],
         },
-        plugins: [
-            resolve(),
-            externals(),
-            useEsbuild
-                ? esbuild()
-                : typescript({
-                    noEmitOnError: useThrowOnError,
-                    outDir: 'dist/cjs',
-                    removeComments: true
+        // CJS build
+        {
+            input: 'src/index.ts',
+            output: {
+                format: 'cjs',
+                dir: 'dist/cjs',
+                entryFileNames: '[name].js', // .cjs
+                strict: true,
+                generatedCode: {
+                    constBindings: true,
+                },
+                preserveModules: true,
+                sourcemap: false,
+            },
+            plugins: [
+                nodeResolve(),
+                nodeExternals(),
+                esbuild(),
+                // NOTE: Babel转义，兼容性支持
+                babel({
+                    presets: ['@babel/preset-env', "@babel/preset-typescript"],
+                    babelHelpers: "bundled",
+                    exclude:"node_modules/**",
+                    extensions: ['.js', '.ts'],
                 }),
-            // NOTE: Babel转义，兼容性支持
-            babel({
-                presets: ['@babel/preset-env', "@babel/preset-typescript"],
-                babelHelpers: "bundled",
-                exclude:"node_modules/**",
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            }),
-        ]
-    },
-    // ESM builds
-    {
-        input: 'src/index.ts',
-        output: {
-            dir: 'dist/esm',
-            format: 'es',
-            generatedCode: {
-                constBindings: usePreferConst
-            },
-            preserveModules: usePreserveModules,
-            strict: useStrict,
-            entryFileNames: '[name].js', // .mjs
-            sourcemap: useSourceMap
+            ]
         },
-        plugins: [
-            externals(),
-            useEsbuild
-                ? esbuild()
-                : typescript({
-                    noEmitOnError: useThrowOnError,
-                    outDir: 'dist/esm',
-                    removeComments: true
-                })
-        ]
-    },
+        // ESM build
+        {
+            input: 'src/index.ts',
+            output: {
+                format: 'es',
+                dir: 'dist/esm',
+                entryFileNames: '[name].js', // .mjs
+                strict: true,
+                generatedCode: {
+                    constBindings: true,
+                },
+                preserveModules: true,
+                sourcemap: false,
+            },
+            plugins: [
+                nodeExternals(),
+                esbuild(),
+            ]
+        },
+    ];
+
     // CDN build
-    {
-        input: 'src/index.ts',
-        output: [
-            // iife
-            {
-                format: 'iife',
-                generatedCode: {
-                    constBindings: usePreferConst
+    if (useCdnBuild) {
+        config.push({
+            input: 'src/index.ts',
+            output: [
+                // iife
+                {
+                    format: 'iife',
+                    file: 'dist/cdn/eUtils.min.js',
+                    name: 'eUtils',
+                    generatedCode: { // es2015
+                        constBindings: true,
+                    },
+                    preserveModules: false,
+                    strict: true,
+                    sourcemap: false,
+                    plugins: [minify()],
                 },
-                preserveModules: false,
-                strict: useStrict,
-                file: 'cdn/eUtils.js',
-                name: 'eUtils',
-                sourcemap: false
-            },
-            {
-                format: 'iife',
-                generatedCode: {
-                    constBindings: usePreferConst
+                // esm
+                {
+                    format: 'es',
+                    file: 'dist/cdn/eUtils.esm.min.js',
+                    name: 'eUtils',
+                    generatedCode: {
+                        constBindings: true,
+                    },
+                    preserveModules: false,
+                    strict: true,
+                    sourcemap: false,
+                    plugins: [minify()],
                 },
-                preserveModules: false,
-                strict: useStrict,
-                file: 'cdn/eUtils.min.js',
-                name: 'eUtils',
-                sourcemap: false,
-                plugins: [minify()]
-            },
-            // esm
-            {
-                format: 'es',
-                generatedCode: {
-                    constBindings: usePreferConst
-                },
-                preserveModules: false,
-                strict: useStrict,
-                file: 'cdn/eUtils.esm.js',
-                sourcemap: false
-            },
-            {
-                format: 'es',
-                generatedCode: {
-                    constBindings: usePreferConst
-                },
-                preserveModules: false,
-                strict: useStrict,
-                file: 'cdn/eUtils.esm.min.js',
-                name: 'eUtils',
-                sourcemap: false,
-                plugins: [minify()]
-            },
-            // umd
-            {
-                format: 'umd',
-                generatedCode: {
-                    constBindings: usePreferConst
-                },
-                preserveModules: false,
-                strict: useStrict,
-                file: 'cdn/eUtils.umd.js',
-                name: 'eUtils',
-                sourcemap: false
-            },
-            {
-                format: 'umd',
-                generatedCode: {
-                    constBindings: usePreferConst
-                },
-                preserveModules: false,
-                strict: useStrict,
-                file: 'cdn/eUtils.umd.min.js',
-                name: 'eUtils',
-                sourcemap: false,
-                plugins: [minify()]
-            }
-        ],
-        plugins: [
-            resolve(),
-            externals(),
-            useEsbuild
-                ? esbuild()
-                : typescript({
-                    noEmitOnError: useThrowOnError,
-                    outDir: 'cdn',
-                    removeComments: true
+                // umd
+                {
+                    format: 'umd',
+                    file: 'dist/cdn/eUtils.umd.min.js',
+                    name: 'eUtils',
+                    generatedCode: {
+                        constBindings: true,
+                    },
+                    preserveModules: false,
+                    strict: true,
+                    sourcemap: false,
+                    plugins: [minify()],
+                }
+            ],
+            plugins: [
+                nodeResolve(),
+                nodeExternals(),
+                esbuild(),
+                // NOTE: Babel转义，兼容性支持
+                babel({
+                    presets: ['@babel/preset-env', "@babel/preset-typescript"],
+                    babelHelpers: "bundled",
+                    exclude:"node_modules/**",
+                    extensions: ['.js', '.ts'],
                 }),
-            // NOTE: Babel转义，兼容性支持
-            babel({
-                presets: ['@babel/preset-env', "@babel/preset-typescript"],
-                babelHelpers: "bundled",
-                exclude:"node_modules/**",
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            }),
-        ]
+            ]
+        })
     }
-]
+
+    return config;
+}
