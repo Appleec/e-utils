5a47cb0db9dab1c281a658c074af5be8d8230ccc
appleex <771992522@qq.com>
2025-02-19 16:53:34 +0800

    refactor: 更新 dom 与 string 方法分类

---
+++ src/dom.ts
@@ -0,0 +1,8 @@
+/**
+ * DOM
+ */
+export { default as isElement } from './isElement';
+
+export { default as hasClass } from './hasClass';
+export { default as addClass } from './addClass';
+export { default as removeClass } from './removeClass';
--- src/string.ts
+++ src/string.ts
@@ -1,12 +1,15 @@
 /**
- * @author appleex
- * @date 2024-08-25 14:36
+ * string
  */
-
-// string
-export { default as trim } from './trim';
 export { default as toString } from './toString';
+export { default as trim } from './trim';
+export { default as trimStart } from './trimStart';
+export { default as trimEnd } from './trimEnd';
+
 export { default as toUpper } from './toUpper';
 export { default as toUpperFirst } from './toUpperFirst';
 export { default as toLower } from './toLower';
 export { default as toLowerFirst } from './toLowerFirst';
+
+export { default as startsWith } from './startsWith';
+export { default as endsWith } from './endsWith';
